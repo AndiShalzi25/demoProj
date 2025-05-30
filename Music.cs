c2c09302a2aac42b989970f02718d5348ac9fbea
USER <USER@SWITCH-PC>
2025-05-30 14:55:38 +0200

    new ones

--- Music.cs
+++ Music.cs
@@ -7,12 +7,20 @@
 
     public string Genre { get; set; }
 
-    
+    public int Age { get; set; }
 
-public Music(string artist, string topSong, string genre)
-{
-    Artist = artist;
-    TopSong = topSong;
-    Genre = genre;
-}
+    public string Nationality { get; set; }
+
+    public string Religion { get; set; }
+
+
+    public Music(string artist, string topSong, string genre, int age, string nationality, string religion)
+    {
+        Artist = artist;
+        TopSong = topSong;
+        Genre = genre;
+        Age = age;
+        Nationality = nationality;
+        Religion = religion;
+    }
 }
