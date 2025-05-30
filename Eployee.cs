614315673ec085927ca1486c822cec798c7d6bf4
USER <USER@DESKTOP-E3IVCBJ>
2025-05-30 14:37:03 +0200

    added a new class

---
+++ Eployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoProj
+{
+    public class Eployee
+    {
+        private int Id { get; set; }
+        public string FullName { get; set; }
+
+        public int Age { get; set; }
+
+
+        public string Gender { get; set; }
+
+
+        public Eployee(string fullName, int age, string gender)
+        {
+            FullName = fullName;
+            Age = age;
+            Gender = gender;
+        }
+
+    }
+
+   
+}
